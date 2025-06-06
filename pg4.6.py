7640b55d294a76f329f4497e26299bd1514e2321
Viswateja <vj001437@gmail.com>
2025-06-06 16:36:51 +0530

    Add files via upload

---
+++ pg4.6.py
@@ -0,0 +1,14 @@
+def add(x,y):
+    return x+y
+def subtraction(x,y):
+    return x-y
+def multiply(x,y):
+    return x*y
+def divide(x,y):
+    return x/y
+num1=float(input("Enter the first number:"))
+num2=float(input("Enter the second number:"))
+print("Addition:",add(num1,num2))
+print("Subtraction:",subtraction(num1,num2))
+print("multiplication:",multiply(num1,num2))
+print("division:",divide(num1,num2))
